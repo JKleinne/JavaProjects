81805548f66ad06fd4ed6ee67f22dccbece00551
JKleinne <jkleinne@protonmail.com>
2021-10-05 18:36:47 -0400

    Added String path for each ComponentType enum values

--- lab1-simulation/src/network/utilities/ComponentType.java
+++ lab1-simulation/src/network/utilities/ComponentType.java
@@ -1,8 +1,18 @@
 package network.utilities;
 
 public enum ComponentType {
-    METAL,
-    WING,
-    MOTOR,
-    PLANE
+    METAL("src/ressources/metal.png"),
+    WING("src/ressources/aile.png"),
+    MOTOR("src/ressources/moteur.png"),
+    PLANE("src/ressources/avion.png");
+
+    private final String path;
+
+    ComponentType(final String path) {
+        this.path = path;
+    }
+
+    public String getIconPath() {
+        return path;
+    }
 }
