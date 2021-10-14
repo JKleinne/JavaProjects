b595ed2b5148cc94e52e6c245debb93705711cfb
JKleinne <jkleinne@protonmail.com>
2021-10-14 16:47:22 -0400

    Javadoc: utilities

--- lab1-simulation/src/network/utilities/ComponentType.java
+++ lab1-simulation/src/network/utilities/ComponentType.java
@@ -1,5 +1,30 @@
+/******************************************************
+ Cours:   LOG121
+ Session: A2021
+ Groupe: 04
+ Projet: Laboratoire #1
+ Étudiant: Jonnie Klein Quezada
+
+
+ Professeur : Benoit Galarneau
+ Nom du fichier: ComponentType.java
+ Date créé: 2021-09-24
+ Date dern. modif. 2021-10-05
+ *******************************************************
+ Historique des modifications (git)
+ *******************************************************
+ JKleinne 10/5/21, 6:36 PM Added String path for each ComponentType enum values
+ JKleinne 9/25/21, 12:01 AM Renamed Material-related classes, attributes, etc. to the correct nomenclature Metal
+ JKleinne 9/24/21, 11:15 PM Refactored Component -> class to record
+ *******************************************************/
+
 package network.utilities;
 
+/**
+ * Enum qui représente les différents types de composants {@link network.records.Component} ainsi que le chemin de l'icône associé
+ * @author Jonnie Klein Quezada
+ * @since 2021-09-24
+ */
 public enum ComponentType {
     METAL("src/ressources/metal.png"),
     WING("src/ressources/aile.png"),
@@ -8,10 +33,18 @@ public enum ComponentType {
 
     private final String path;
 
+    /**
+     * Constructeur qui prend 1 argument
+     * @param path Le chemin de l'icône
+     */
     ComponentType(final String path) {
         this.path = path;
     }
 
+    /**
+     * Getter pout path
+     * @return Le chemin de l'icône
+     */
     public String getIconPath() {
         return path;
     }
--- lab1-simulation/src/network/utilities/IndicatorStatus.java
+++ lab1-simulation/src/network/utilities/IndicatorStatus.java
@@ -1,5 +1,30 @@
+/******************************************************
+ Cours:   LOG121
+ Session: A2021
+ Groupe: 04
+ Projet: Laboratoire #1
+ Étudiant: Jonnie Klein Quezada
+
+
+ Professeur : Benoit Galarneau
+ Nom du fichier: IndicatorStatus.java
+ Date créé: 2021-09-19
+ Date dern. modif. 2021-10-09
+ *******************************************************
+ Historique des modifications (git)
+ *******************************************************
+ JKleinne 10/9/21, 11:15 PM Added Facility production status icon and create new Component only when status is FULL
+ JKleinne 10/5/21, 5:23 PM Added int values for IndicatorStatus enum
+ JKleinne 9/19/21, 3:51 PM Warehouse, finish skeleton structure
+ *******************************************************/
+
 package network.utilities;
 
+/**
+ * Enum qui représente les différents statuts d'une instance de {@link network.facilities.Facility}
+ * @author Jonnie Klein Quezada
+ * @since 2021-09-19
+ */
 public enum IndicatorStatus {
     EMPTY(0),
     ONE_THIRD(1),
@@ -8,14 +33,26 @@ public enum IndicatorStatus {
 
     private final int index;
 
+    /**
+     * Constructeur qui prend 1 argument
+     * @param index L'index de chaque statut
+     */
     IndicatorStatus(final int index) {
         this.index = index;
     }
 
+    /**
+     * Getter pour index
+     * @return
+     */
     public int getIndex() {
         return index;
     }
 
+    /**
+     * Itère jusqu'à et renvoie le statut suivant
+     * @return Le prochain statut
+     */
     public IndicatorStatus getNext() {
         return ordinal() != values().length - 1 ? values()[ordinal() + 1] : null;
     }
